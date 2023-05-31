99fe11e0bbb16f263b48dad1d863648f69eb208f
cintia89 <cintia.silvadeooliveira@gmail.com>
2023-05-31 16:11:15 -0300

    feat: inicia o projeto criando o componente header

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,18 @@
+import "./header.css"
+
+export const Header = () => {
+    return (<>
+    <div className="header-components"> 
+    <div>Logo</div>
+    <div> 
+        <ul>
+            <li>menu</li>
+            <li>menu</li>
+            <li>menu</li>
+            <li>menu</li>
+        </ul>
+    </div>
+    </div>
+    </>
+    )
+}
